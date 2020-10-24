8d270779dd1b6f991269500fbe8c554a8aa67948
Zorca Orcinus <vs@zorca.org>
2020-10-24 21:33:24 +0300

    working upload

--- indaupload.php
+++ indaupload.php
@@ -34,6 +34,43 @@ $indauploadPlugin->onDeactivation(
 
 $indauploadPlugin->boot(
     function( $plugin ) {
-        // Do something when the plugins_loaded hook is fired.
+        add_action('admin_menu', 'my_admin_menu');
+        function my_admin_menu() {
+            add_menu_page('Импорт баллов', 'Импорт баллов', 'edit_others_posts', 'import-points.php', 'print_page_function');
+            function print_page_function() {
+                if(isset($_POST['submit_upload'])) {
+
+                    if($_FILES['file']['name'] != ''){
+                        $uploadedfile = $_FILES['file'];
+                        $upload_overrides = array( 'test_form' => false );
+
+                        $movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
+                        $imageurl = "";
+                        if ( $movefile && ! isset( $movefile['error'] ) ) {
+                            $upload_dir = wp_upload_dir();
+                            rename($movefile['file'], $upload_dir['basedir']  . WPALLIMPORT_POINTS_PATH . 'points.csv');
+                            echo "Файл успешно загружен и будет импортирован автоматически по расписанию.";
+                        } else {
+                            echo $movefile['error'];
+                        }
+                    }
+
+                }
+                ?>
+                <form method='post' action='' name='myform' enctype='multipart/form-data'>
+                    <table>
+                        <tr>
+                            <td>Upload file</td>
+                            <td><input type='file' name='file'></td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td><input type='submit' name='submit_upload' value='Загрузить файл формата CSV'></td>
+                        </tr>
+                    </table>
+                </form>
+                <?php
+            }
+        }
     }
 );
