b04c6f959603b7da247136373e0e5232b41fe2b1
Zorca Orcinus <vs@zorca.org>
2020-10-25 14:19:52 +0300

    add styles

--- indaupload.php
+++ indaupload.php
@@ -34,45 +34,57 @@ $indauploadPlugin->onDeactivation(
 
 $indauploadPlugin->boot(
     function( $plugin ) {
+        add_action('admin_head', 'admin_stylesheet_tailwindcss');
+        function admin_stylesheet_tailwindcss() {
+            wp_enqueue_style("style-admin-tailwindcss", 'https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css');
+        }
+
         add_action('admin_menu', 'my_admin_menu');
         function my_admin_menu() {
             add_menu_page('Импорт баллов', 'Импорт баллов', 'edit_others_posts', 'import-points.php', 'print_page_function');
             function print_page_function() {
+                ?>
+                <div class="wrap">
+                    <h2>Загрузка файла импорта сотрудников и их баллов</h2>
+                    <form class="mt-8" method='post' action='' name='myform' enctype='multipart/form-data'>
+                        <div class="w-64 flex flex-wrap items-center justify-center bg-grey-lighter">
+                            <label class="flex flex-col items-center px-4 py-6 mb-4 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
+                                <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+                                </svg>
+                                <span class="mt-2 text-base leading-normal">Выберите файл CSV</span>
+                                <input type='file' name='file' class="hidden" />
+                            </label>
+                            <input class="border p-4 text-xl text-center" type='submit' name='submit_upload' value='Загрузить файл CSV'>
+                        </div>
+                    </form>
+                </div>
+                <?php
                 if(isset($_POST['submit_upload'])) {
 
                     if($_FILES['file']['name'] != ''){
                         $uploadedfile = $_FILES['file'];
-                        $upload_overrides = array( 'test_form' => false );
 
-                        $movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
-                        $imageurl = "";
-                        if ( $movefile && ! isset( $movefile['error'] ) ) {
-                            $upload_dir = wp_upload_dir();
-                            rename($movefile['file'], WPALLIMPORT_POINTS_PATH);
-                            echo "Файл успешно загружен и будет импортирован автоматически по расписанию.";
+                        $mimes = array('application/vnd.ms-excel','text/plain','text/csv','text/tsv');
+                        if(in_array($_FILES['file']['type'],$mimes)){
+                            $upload_overrides = array( 'test_form' => false );
+
+                            $movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
+                            $imageurl = "";
+                            if ( $movefile && ! isset( $movefile['error'] ) ) {
+                                $upload_dir = wp_upload_dir();
+                                rename($movefile['file'], WPALLIMPORT_POINTS_PATH);
+                                echo "Файл успешно загружен и будет импортирован автоматически по расписанию.";
+                            } else {
+                                echo $movefile['error'];
+                            }
                         } else {
-                            echo $movefile['error'];
+                            echo 'Загруженный файл имеет некорректный формат';
                         }
+
                     }
 
                 }
-                ?>
-                <div class="wrap">
-                    <h2>Загрузка файла импорта сотрудников и их баллов</h2>
-                    <form method='post' action='' name='myform' enctype='multipart/form-data'>
-                        <table>
-                            <tr>
-                                <td>Upload file</td>
-                                <td><input type='file' name='file'></td>
-                            </tr>
-                            <tr>
-                                <td>&nbsp;</td>
-                                <td><input type='submit' name='submit_upload' value='Загрузить файл формата CSV'></td>
-                            </tr>
-                        </table>
-                    </form>
-                </div>
-                <?php
             }
         }
     }
