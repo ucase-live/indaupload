f37ed2ba94f3d80601127e5f3c7ec265f36ef0c1
Zorca Orcinus <vs@zorca.org>
2020-11-04 11:56:24 +0300

    fix: styles

--- indaupload.php
+++ indaupload.php
@@ -48,14 +48,14 @@ $indauploadPlugin->boot(
                     <h2>Загрузка файла импорта сотрудников и их баллов</h2>
                     <form class="mt-8" method='post' action='' name='myform' enctype='multipart/form-data'>
                         <div class="w-64 flex flex-wrap items-center justify-center bg-grey-lighter">
-                            <label class="flex flex-col items-center px-4 py-6 mb-4 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
+                            <label class="w-full flex flex-col items-center px-4 py-6 mb-4 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase transition border border-blue-500 cursor-pointer hover:bg-blue-500 hover:text-white">
                                 <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
                                 </svg>
                                 <span class="mt-2 text-base leading-normal">Выберите файл CSV</span>
                                 <input type='file' name='file' class="hidden" />
                             </label>
-                            <input class="border p-4 text-xl text-center" type='submit' name='submit_upload' value='Загрузить файл CSV'>
+                            <input class="w-full button button-primary rounded-lg p-2 text-xl text-center" type='submit' name='submit_upload' value='Загрузить файл CSV'>
                         </div>
                     </form>
                 </div>
